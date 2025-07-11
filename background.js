04b53379819342c36722de1ddab56675490782fe
Tuvi11 <soujanyamaharudra02@gmail.com>
2025-07-11 19:48:40 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,11 @@
+
+chrome.runtime.onInstalled.addListener(() => {
+  
+  chrome.storage.sync.get(["geminiApiKey"], (result) => {
+    if (!result.geminiApiKey) {
+      chrome.tabs.create({
+        url: "options.html",
+      });
+    }
+  });
+});
